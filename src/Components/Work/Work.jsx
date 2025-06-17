831ddcf6c2f2d79a64cc26664294cc279950067a
joyasaha1004 <joyasaha440@gmail.com>
2025-06-17 21:56:57 +0600

    add all changes

--- src/Components/Work/Work.jsx
+++ src/Components/Work/Work.jsx
@@ -97,7 +97,7 @@ const Work = () => {
                       </div></div>
                   
                </div>
-                       <div className="flex flex-wrap pt-2 lg:relative lg:justify-end lg:-mt-24 lg:px-10 lg:gap-10 justify-center gap-4 sm:gap-8 md:gap-16">
+                       <div className="flex flex-wrap pt-2 lg:relative lg:justify-end px-2 lg:-mt-24 lg:px-10 lg:gap-10 justify-center gap-4 sm:gap-8 md:gap-16">
                            <div className="plus-point align-center">
                             <div className='plus-point-img'>
                                <img src={leaf} className=""></img>
